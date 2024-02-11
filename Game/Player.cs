ec88b9fb462714a0400eec0328d1905fe1b7e0ae
chriswde <17209477+chriswde@users.noreply.github.com>
2024-02-12 00:04:23 +0100

    Randomized player icons

--- Game/Player.cs
+++ Game/Player.cs
@@ -5,13 +5,18 @@ namespace OhaseApi.Game
     public string PlayerId { get; private set; }
     public string Nick { get; set; }
     public string? NameToGuess { get; set; }
+    public string Icon { get; set; }
     public bool ActiveTurn { get; set; }
+
+    private string[] emojiWhitelist = new [] {"👴", "🤡", "😏", "👸", "👮‍♀️", "💂‍♀️", "🧑‍🌾", "🧑‍🚀", "🫃", "🤢", "💃", "🧘", "🦼", "🌚", "👽"};
     public Player(string nick, string playerid)
     {
       this.Nick = nick;
       this.PlayerId = playerid;
-      
       this.ActiveTurn = false;
+      this.Icon = this.emojiWhitelist[new Random().Next(0, this.emojiWhitelist.Length)];
     }
+
+
   }
 }
